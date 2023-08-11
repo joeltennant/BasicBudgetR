99eb2acf0550729a54ad05dd306fb883f1d324d1
Joel Tennant <joel_tennant@yahoo.com>
2023-08-11 14:16:36 -0600

    Create Signup handler

---
+++ src/Server/BasicBudgetR.Server.Application/Handlers/User/SignUp.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicBudgetR.Server.Application.Handlers.User;
+public class SignUp
+{
+    public record Request : IRequest<Response>
+    {
+    }
+
+    public record Response(long AccountId);
+
+    public class Handler : BaseHandler, IRequestHandler<Request, Response>
+    {
+        public Handler(BudgetRDbContext context, CurrentProcess currentProcess)
+            : base(context, currentProcess)
+        {
+        }
+
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            return new Response(1);
+        }
+    }
+}
