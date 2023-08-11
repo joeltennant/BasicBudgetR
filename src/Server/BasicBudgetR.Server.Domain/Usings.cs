d2446faa60bc17a49656ec4086d529d5ee1529d1
Joel Tennant <joel_tennant@yahoo.com>
2023-08-11 13:50:29 -0600

    Rename Household to HouseHoldConfiguration

--- src/Server/BasicBudgetR.Server.Domain/Usings.cs
+++ src/Server/BasicBudgetR.Server.Domain/Usings.cs
@@ -1,5 +1,5 @@
-﻿global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using Microsoft.EntityFrameworkCore;
-global using BasicBudgetR.Core.Enums;
+﻿global using BasicBudgetR.Core.Enums;
 global using BasicBudgetR.Server.Domain.Entities.ReferenceEntities;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
