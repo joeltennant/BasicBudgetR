c110e316236adffc3b3c7aaee92afe3a11845248
Joel Tennant <joel_tennant@yahoo.com>
2023-09-13 15:14:03 -0600

    Add ExpenseList Page

---
+++ src/BasicBudgetR.Core/Models/ExpenseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicBudgetR.Core.Models;
+public class ExpenseModel
+{
+    public long ExpenseId { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal Amount { get; set; }
+    public bool IsActive { get; set; }
+}
